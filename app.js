8feefd436a768ae3db1bd6758b0ca03d8f2085c7
YASH2204V <yash.v220104@gmail.com>
2024-09-26 05:09:18 +0530

    added googleAuth, little bit styling: Vaibhav

--- app.js
+++ app.js
@@ -1,13 +1,44 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+require("dotenv").config()
+const session = require("express-session")
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'src', 'public')));
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: process.env.EXPRESS_SESSION_SECRET
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set('views', path.join(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: '/auth/google/callback'
+},
+    function (accessToken, refreshToken, profile, done) {
+        // Save user profile to your database if needed
+        return done(null, profile);
+    }));
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
 
 app.get("/", (req, res) => {
     res.render("home");
@@ -17,6 +48,27 @@ app.get("/hotelsAndHouses", (req, res) => {
 });
 app.get("/hotel", (req, res) => {
     res.render("hotel");
+});
+
+
+// Google Authentication Routes
+app.get("/login", (req, res) => {
+    res.render("authO");
+})
+
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+app.get('/auth/google/callback',
+    passport.authenticate('google', { failureRedirect: '/' }),
+    (req, res) => {
+        res.redirect('/profile');
+    }
+);
+
+app.get('/profile', (req, res) => {
+    res.send(`<h1>Profile</h1><pre>${JSON.stringify(req.user, null, 2)}</pre>`);
 });
 
 app.listen(3000, ()=>{
